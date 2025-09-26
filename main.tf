09845e393e76fe9c74d90cc526b12ac9b82eb45f
vishnuvardhan25-create <vishnulinux25@gmail.com>
2025-09-26 19:22:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "ap-south-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI ID (example for us-east-1)
-  instance_type = "t2.micro"
+  ami           = "ami-01b6d88af12965bb6" # Amazon Linux 3 AMI ID (example for us-east-1)
+  instance_type = "t3.micro"
   count         = 5   # <--- Creates 5 instances
 
   tags = {
